0421a40aad6b2bf4bbc5608dd7ba4fb2737f6d14
Chinthala Pallavi <chinthalapallavi12@gmail.com>
2025-10-04 13:09:27 +0530

    Update Question.java

--- model/Question.java
+++ model/Question.java
@@ -1,1 +1,27 @@
-/ Question.java placeholder
+package model;
+
+public class Question {
+    private String questionText;
+    private String answer;
+
+    // Constructor
+    public Question(String questionText, String answer) {
+        this.questionText = questionText;
+        this.answer = answer;
+    }
+
+    // Getter for question text
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    // Getter for answer
+    public String getAnswer() {
+        return answer;
+    }
+
+    // Check if the user's answer is correct
+    public boolean isCorrect(String userAnswer) {
+        return answer.equalsIgnoreCase(userAnswer.trim());
+    }
+}
